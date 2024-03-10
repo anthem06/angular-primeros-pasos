a54b5e0874e455312b852d135a7ed3f78ede52ac
Antonio <hernandezgreen@gmail.com>
2024-03-10 17:20:35 -0600

    Se convirtio el constructor del servicio en privado por mejor practica y se generaron los metodos para compartir los en el html

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -22,7 +22,7 @@ export class DbzService {
   }
   ];
 
-  onNewCharacter( character: Character): void {
+  addCharacter( character: Character): void {
 
     const newCharacter: Character = {
       id: uuid(),
@@ -36,7 +36,7 @@ export class DbzService {
   //   this.characters.splice(index, 1);
   // }
 
-  deleteCharacterById(id: string): void{
+ deleteCharacterById (id: string): void{
     this.characters = this.characters.filter(character => character.id !== id);
   }
 }
